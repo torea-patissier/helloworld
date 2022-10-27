1e8dfd693c46f47527bdffb44069d5e9fa799674
Torea Patissier <p.torea@icloud.com>
2022-10-27 14:33:53 +0200

    feat: Feed the cache
    Look at the serviceworker
    self.addEventListener(install..)
    
    -run nox tsc --watch to run typescript
    check google chrome console
    
    if needed Applications > Cache > Cache storage to see if the files specify in the addAll are present (in this case index.html...)

--- public/serviceworker.ts
+++ public/serviceworker.ts
@@ -1,7 +1,10 @@
 console.log('I am a service worker');
 
-self.addEventListener('install', ()=>{
+self.addEventListener('install', async ()=>{
     console.log('The worker is installed');
+    const cache =  await caches.open('hello-cache')
+    await cache.addAll(['index.html','/','assets/cacahuete.jpeg'])
+    console.log('The data is cached')
 })
 
 self.addEventListener('activate',()=>{
